f97c4e14be7e7eb838c0f3a850e883403d32bcce
mediamakermatt <mediamakermatt@gmail.com>
2026-01-01 22:42:13 -0500

    work on scroll nav

--- js/navigation.js
+++ js/navigation.js
@@ -6,17 +6,23 @@ window.addEventListener('scroll', () => {
     let width = window.innerWidth;
 
     if (currentScrollY >= lastScrollY && width <= 768) {
-        document.getElementById('nav').classList.remove('top-0', 'sticky');
+
+        document.getElementById('nav').classList.remove('top-0', 'sticky', 'text-base');
+        document.getElementById('nav').classList.add('h-0', 'text-[0]');
+
         document.getElementById('footer').classList.remove('-bottom-8', 'fixed');
-        document.getElementById('nav').classList.add('-top-34', 'fixed');
         document.getElementById('footer').classList.add('bottom-0', 'sticky');
         lastScrollY = currentScrollY;
+
     } else if (currentScrollY < lastScrollY && width <= 768) {
-        document.getElementById('nav').classList.remove('-top-34', 'fixed');
+
+        document.getElementById('nav').classList.remove('h-0', 'text-[0]');
+        document.getElementById('nav').classList.add('top-0', 'sticky', 'text-base');
+
         document.getElementById('footer').classList.remove('bottom-0', 'sticky');
-        document.getElementById('nav').classList.add('top-0', 'sticky');
         document.getElementById('footer').classList.add('-bottom-8', 'fixed');
         lastScrollY = currentScrollY;
+
     }
 
 });
